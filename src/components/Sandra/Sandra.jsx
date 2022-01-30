c8166ba4096ef60e93d52d63aa6919e0155e0008
SandraMey <sandra.meyniel@gmail.com>
2022-01-30 15:54:28 +0100

    first version

--- src/components/Sandra/Sandra.jsx
+++ src/components/Sandra/Sandra.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './Sandra.css';
+import {lému} from '../../assets/sandraimg.jpg';
 
 const Sandra = () => {
     return (
-        <div>
-            Coucou Sandra
-        </div>
+        <div className='containerSandra'>
+            <div class="merci">
+                <div className='squ'>M</div>
+                <div className='squ'>E</div>
+                <div className='squ'>R</div>
+                <div className='squ'>C</div>
+                <div className='squ'>I</div>
+            </div>
+            <div class="francois">
+                <div className='fr'>
+                   <h1 className='titlefr'>FRANCOIS</h1>
+                </div>
+            </div>
+      </div>
     )
 }
 
