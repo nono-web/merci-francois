1ce40ea0ccee312b2f1b1ab1bcfeed0071f3f66a
Vincent Doree <vinz77@hotmail.com>
2022-01-31 09:44:55 +0100

    Component Vincent

--- src/components/Vincent/Vincent.jsx
+++ src/components/Vincent/Vincent.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Vincent.css';
+
 
 const Vincent = () => {
+    const [testing, setTesting] = useState("");
+    const [testing2, setTesting2] = useState("Clique ici François!")
+    
+    const handleClick = () => {
+    setTesting ("dv");
+    setTesting2 ("");
+}
+
     return (
         <div>
-            Coucou Vincent
+            <p className="pp"> Merci François !! Pour cette Formation !!</p>
+            <div>
+            <button className={testing} onClick={handleClick}>{testing2}</button>
+            </div>
+            
         </div>
     )
 }
 
-export default Vincent
+export default Vincent;
