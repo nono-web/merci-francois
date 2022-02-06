d413de40bf08f03bbd0d95774309beca3bfcf6e1
LucilleBernade <bernadelucille@gmail.com>
2022-02-06 23:00:40 +0100

    Lucille

--- src/components/Lucille/Lucille.jsx
+++ src/components/Lucille/Lucille.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import "./Lucille.css";
 
 const Lucille = () => {
-    return (
-        <div className='prenom'>
-            Lucille
-        </div>
-    )
-}
+  return (
+    <>
+      <div className="prenom">Lucille</div>
+      <div className="back">
+        <div className="thank">Merci François !!!</div>
+		<div className="thank2">(Ceci est un NFT)</div>
+      </div>
+    </>
+  )
+};
 
-export default Lucille
+export default Lucille;
